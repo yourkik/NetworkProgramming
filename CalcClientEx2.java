86911f2909319acff3166dc5ebb5e86920fb9f8d
yourkik <73531665+yourkik@users.noreply.github.com>
2023-11-07 20:55:58 +0900

    Change output format statusCode + meaning

--- CalcClientEx2.java
+++ CalcClientEx2.java
@@ -9,11 +9,12 @@ public class CalcClientEx2 {
         Socket socket = null;
         Scanner scanner = new Scanner(System.in);
         try {
-            //default IP, port number, 따로 저장하여 사용
+            // default IP, port number, 따로 저장하여 사용
             String serverIP = "localhost";
             int serverPort = 9999;
 
-            //Server_info.dat을 통해 server 정보를 가져오는 코드 try/catch를 사용해 파일 정보가 잘못되거나 이름이 잘못됬을 때 예외 처리
+            // Server_info.dat을 통해 server 정보를 가져오는 코드 try/catch를 사용해 파일 정보가 잘못되거나 이름이 잘못됬을 때
+            // 예외 처리
             try {
                 File configFile = new File("Server_info.dat");
                 if (configFile.exists()) {
@@ -34,19 +35,25 @@ public class CalcClientEx2 {
             socket = new Socket(serverIP, serverPort);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            while (true) {
-                System.out.print("계산식(빈칸으로 띄어 입력,예:24 + 42, bye 입력시 종료)>>"); // 프롬프트
-                String outputMessage = scanner.nextLine(); // 키보드에서 수식 읽기
-                if (outputMessage.equalsIgnoreCase("bye")) {
-                    out.write(outputMessage + "\n"); // "bye" 문자열 전송
+                while (true) {
+                    System.out.print("계산식(빈칸으로 띄어 입력,예:24 + 42, bye 입력시 종료)>>"); // 프롬프트
+                    String outputMessage = scanner.nextLine(); // 키보드에서 수식 읽기
+                    if (outputMessage.equalsIgnoreCase("bye")) {
+                        out.write(outputMessage + "\n"); // "bye" 문자열 전송
+                        out.flush();
+                        break; // 사용자가 "bye"를 입력한 경우 서버로 전송 후 연결 종료
+                    }
+                    out.write(outputMessage + "\n"); // 키보드에서 읽은 수식 문자열 전송
                     out.flush();
-                    break; // 사용자가 "bye"를 입력한 경우 서버로 전송 후 연결 종료
+                    String inputMessage = in.readLine(); // 서버로부터 계산 결과 수신
+                    String splitMessage[] = inputMessage.split(",");
+                    if (splitMessage[0].equals("200")) {
+                        System.out.println("Result : " + splitMessage[1] + "(Status Code : " + splitMessage[0] + ")");
+                    } else {
+                        System.out.println("ERROR : " + splitMessage[1] + "(Status Code : " + splitMessage[0] + ")");
+                    }
                 }
-                out.write(outputMessage + "\n"); // 키보드에서 읽은 수식 문자열 전송
-                out.flush();
-                String inputMessage = in.readLine(); // 서버로부터 계산 결과 수신
-                System.out.println("Result : " + inputMessage);
-            }
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         } finally {
@@ -55,7 +62,7 @@ public class CalcClientEx2 {
                 if (socket != null)
                     socket.close(); // 클라이언트 소켓 닫기
             } catch (IOException e) {
-                System.out.println("Erro : Error in chating with server.");
+                System.out.println("Error : Error in chating with server.");
             }
         }
     }
--- CalcServerExMultiple.java
+++ CalcServerExMultiple.java
@@ -1,44 +1,55 @@
 //Server_info.dat을 client에서 생성해야하는지 Server에서 생성해야하는지 의문
-//Protocol이 상태 메세지를 의미? -> 이에 따른 상태 코드를 출력하도록 변경할지 선택 필요
+//Protocol이 상태 메세지를 의미? -> 이에 따른 상태 코드를 출력하도록 변경할지 선택 필요 -> 변경 완료
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
 public class CalcServerExMultiple {
-    //예외는 message를 통해 구현(하나의 예외로 여러 예외를 처리하기 위함 -> DivisionByZero의 경우 변경 가능하나 통일함)
-    //예외 정의 : 유효하지 않은 계산식
+    int statusCode = 0;
+
+    // 예외는 message를 통해 구현(하나의 예외로 여러 예외를 처리하기 위함 -> DivisionByZero의 경우 변경 가능하나 통일함)
+    // 11.07 message 형식 변경 : message는 StatusCode,meaning의 형태로 변경됨 Success의 경우 StatusCode에 200 그 외는 각자에 맞는 Status_Code가 들어감
+    // 예외 정의 : 유효하지 않은 계산식
     public static class InvalidExpressionException extends Exception {
         public InvalidExpressionException(String message) {
             super(message);
         }
     }
 
-    //예외 정의 : 0으로 나누었을 때
+    // 예외 정의 : 0으로 나누었을 때
     public static class DivisionByZeroException extends Exception {
         public DivisionByZeroException(String message) {
             super(message);
         }
     }
 
-    //명령 및 응답 프로토콜 정의
+    // 명령 및 응답 프로토콜 정의
     public static final String COMMAND_ADD = "ADD";
     public static final String COMMAND_SUBTRACT = "SUB";
     public static final String COMMAND_MULTIPLY = "MUL";
     public static final String COMMAND_DIVIDE = "DIV";
-    public static final String RESPONSE_ERROR = "ERROR";
 
-    //Input : exp(계산식이 저장된 string)
-    //Output : res(계산 진행 후의 값을 저장하는 String)
-    //계산을 진행하는 함수로 들어온 exp(계산식)을 Token을 통해 공백을 기준으로 분할 후 숫자로 변환해 계산 이후 출력
-    //InvalidException과 DivisionByZeroException을 통해 예외 처리
+    // Status code 정의
+    // 상태 코드 정의
+    public static final String STATUS_SUCCESS = "200"; // 성공
+    public static final String STATUS_BAD_FORMAT = "400"; // message 형식이 다름
+    public static final String STATUS_DIVISION_BY_ZERO = "401"; // 0으로 나눔
+    public static final String STATUS_INVALID_EXPRESSION = "402"; // 유효하지 않은 수학 기호
+    public static final String STATUS_UNKNOWN_ERROR = "500"; // 알 수 없는 오류, 현재 사용하지는 않으나 의미상 만듦
+
+    // Input : exp(계산식이 저장된 string)
+    // Output : res(계산 진행 후의 값을 저장하는 String)
+    // 계산을 진행하는 함수로 들어온 exp(계산식)을 Token을 통해 공백을 기준으로 분할 후 숫자로 변환해 계산 이후 출력
+    // InvalidException과 DivisionByZeroException을 통해 예외 처리
     public static String calc(String exp) throws InvalidExpressionException, DivisionByZeroException {
         StringTokenizer st = new StringTokenizer(exp, " ");
         if (st.countTokens() != 3) {
-            //계산식 형태에 예외 발생
-            throw new InvalidExpressionException(RESPONSE_ERROR+" : Invalid expression format : " + exp + ". Enter just format like (a + b)");
+            // 계산식 형태에 예외 발생
+            throw new InvalidExpressionException(
+                    STATUS_BAD_FORMAT + ",Invalid expression format. Enter just format like (a + b)");
         }
 
-        String res="";
+        String res = "";
         String responseType;
         int op1 = Integer.parseInt(st.nextToken());
         String opcode = st.nextToken();
@@ -47,40 +58,41 @@ public class CalcServerExMultiple {
         switch (opcode) {
             case "+":
                 res = Integer.toString(op1 + op2);
-                responseType=COMMAND_ADD;
+                responseType = COMMAND_ADD;
                 break;
             case "-":
                 res = Integer.toString(op1 - op2);
-                responseType=COMMAND_SUBTRACT;
+                responseType = COMMAND_SUBTRACT;
                 break;
             case "*":
                 res = Integer.toString(op1 * op2);
-                responseType=COMMAND_MULTIPLY;
+                responseType = COMMAND_MULTIPLY;
                 break;
             case "/":
                 if (op2 == 0) {
-                    //0으로 나누는 예외
-                    throw new DivisionByZeroException(RESPONSE_ERROR +" : Division by zero is not allowed.");
+                    // 0으로 나누는 예외
+                    throw new DivisionByZeroException(STATUS_DIVISION_BY_ZERO + ",Division by zero is not allowed.");
                 }
                 res = Integer.toString(op1 / op2);
-                responseType=COMMAND_DIVIDE;
+                responseType = COMMAND_DIVIDE;
                 break;
-            //default에서 throw가 발생하면 아래 return을 무시 -> error 발생 -> res에 error를 넣고 이를 검출해 throw 발생
+            // default에서 throw가 발생하면 아래 return을 무시 -> error 발생 -> res에 error를 넣고 이를 검출해
+            // throw 발생
             default:
-                res="error";
-                responseType=RESPONSE_ERROR;
+                res = "error";
+                responseType = STATUS_INVALID_EXPRESSION;
         }
 
-        if(res=="error"){
-            //저장된 기호가 아닐 때 발생하는 예외
-            throw new InvalidExpressionException(responseType + " : Invalid operator(" + opcode+")");
+        if (res == "error") {
+            // 저장된 기호가 아닐 때 발생하는 예외
+            throw new InvalidExpressionException(responseType + ",Invalid operator(" + opcode + ")");
+        } else {
+            return STATUS_SUCCESS + "," + responseType + " : " + res;
         }
-        return responseType+" : "+res;
     }
 
     public static void main(String[] args) {
         ServerSocket listener = null;
-
         try {
             listener = new ServerSocket(9999);
             System.out.println("연결을 기다리고 있습니다.....");
@@ -89,7 +101,7 @@ public class CalcServerExMultiple {
                 Socket socket = listener.accept();
                 System.out.println("연결되었습니다.");
 
-                Thread clientThread = new Thread(new ClientHandler(socket));//socket에 대해서 Thread를 객체를 생성
+                Thread clientThread = new Thread(new ClientHandler(socket));// socket에 대해서 Thread를 객체를 생성
                 clientThread.start();
             }
         } catch (IOException e) {
@@ -107,22 +119,21 @@ public class CalcServerExMultiple {
     private static class ClientHandler implements Runnable {
         private Socket socket;
 
-        //ClientHander에 대한 생성자
+        // ClientHander에 대한 생성자
         public ClientHandler(Socket socket) {
             this.socket = socket;
         }
 
-        //Runnable에 있는 run 함수 override
+        // Runnable에 있는 run 함수 override
         @Override
         public void run() {
             BufferedReader in = null;
             BufferedWriter out = null;
 
             try {
-                //입출력 버퍼, 이를 통해 출력해야 Client에 출력
+                // 입출력 버퍼, 이를 통해 출력해야 Client에 출력
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-
                 while (true) {
                     String inputMessage = in.readLine();
                     if (inputMessage.equalsIgnoreCase("bye")) {
@@ -134,7 +145,7 @@ public class CalcServerExMultiple {
                         String res = calc(inputMessage);
                         out.write(res + "\n");
                     } catch (InvalidExpressionException | DivisionByZeroException e) {
-                        out.write(e.getMessage() + "\n");//\n이 없으면 출력되지 않음
+                        out.write(e.getMessage() + "\n");// \n이 없으면 출력되지 않음
                     }
                     out.flush();
                 }
